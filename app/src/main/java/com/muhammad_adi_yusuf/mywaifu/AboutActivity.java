b8b70455706e1cc220874ee8974207cc07b8084f
kazuiains <kaisaryusuf@gmail.com>
2019-09-14 10:23:58 +0700

    Initial commit

---
+++ app/src/main/java/com/muhammad_adi_yusuf/mywaifu/AboutActivity.java
@@ -0,0 +1,14 @@
+package com.muhammad_adi_yusuf.mywaifu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
